22cc6c00e1e3c27bbaf4184fc39444867f68fea5
Nikita Shvyryaev <n.shvyryaev@vk.team>
2023-05-02 11:27:41 +0600

    MA-12718: force users to use navigator, not router.

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,23 +1,30 @@
 import { useContext } from 'react';
-import { PopoutContext, RouteContext, RouterContext, RouterContextObject } from '../contexts';
-import { Location, Params } from '@remix-run/router';
+import { PopoutContext, RouteContext, RouterContext } from '../contexts';
+import { Location, Params, UNSAFE_invariant as invariant } from '@remix-run/router';
+import { RouteNavigator } from '../services/routeNavigator.type';
 
-export function useRouterContext(): RouterContextObject {
-  return useContext(RouterContext);
+export function useNavigator(): RouteNavigator {
+  const routerContext = useContext(RouterContext);
+  invariant(routerContext, 'You can not use useNavigator hook outside of RouterContext. Make sure calling it inside RouterProvider.');
+  return routerContext.navigator;
 }
 
 export function usePanelParams<T extends string = string>(): Params<T> | undefined {
   const routeContext = useContext(RouteContext);
+  invariant(routeContext, 'You can not use usePanelParams hook outside of RouteContext. Make sure calling it inside RouterProvider.');
   return routeContext.panelMatch?.params;
 }
 
 export function useModalParams<T extends string = string>(): Params<T> | undefined {
   const routeContext = useContext(RouteContext);
+  invariant(routeContext, 'You can not use useModalParams hook outside of RouteContext. Make sure calling it inside RouterProvider.');
   return routeContext.modalMatch?.params;
 }
 
 export function useLocation(): Location {
-  return useContext(RouteContext).state.location;
+  const routeContext = useContext(RouteContext);
+  invariant(routeContext, 'You can not use useLocation hook outside of RouteContext. Make sure calling it inside RouterProvider.');
+  return routeContext.state.location;
 }
 
 export function usePopout(): JSX.Element | null {
