7bbf1ab82e7064fd70ef2edcaf989dd8a296840f
Nikita Shvyryaev <n.shvyryaev@vk.team>
2023-05-15 20:14:01 +0600

    MA-12425: router guide

--- src/type.ts
+++ src/type.ts
@@ -1,7 +1,7 @@
 import { AgnosticIndexRouteObject } from '@remix-run/router';
 
 export interface CommonRouteObject {
-  path?: AgnosticIndexRouteObject['path'];
+  path: AgnosticIndexRouteObject['path'];
 }
 
 export interface PanelWithoutRoot extends CommonRouteObject {
@@ -22,6 +22,9 @@ export interface ModalWithoutRoot extends PanelWithoutRoot {
 export interface ModalWithRoot extends PanelWithRoot {
   modal: string;
 }
+
+export type RouteWithRoot = PanelWithRoot | ModalWithRoot;
+export type RouteWithoutRoot = PanelWithoutRoot | ModalWithoutRoot;
 
 export type InternalRouteConfig = { index: true; id: string };
 export type PageInternal = (PanelWithoutRoot & InternalRouteConfig) | (PanelWithRoot & InternalRouteConfig) | (ModalWithoutRoot & InternalRouteConfig) | (ModalWithRoot & InternalRouteConfig);
