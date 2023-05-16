473ffc39b9ad0c518182a242ef0a09bf376300f7
Nikita Shvyryaev <n.shvyryaev@vk.team>
2023-05-16 19:50:06 +0600

    MA-12425: remove complete React imports, update peer dependency

--- src/contexts.ts
+++ src/contexts.ts
@@ -1,5 +1,5 @@
 import { AgnosticRouteMatch, Router, RouterState } from '@remix-run/router';
-import React from 'react';
+import { createContext } from 'react';
 import { RouteNavigator } from './services/routeNavigator.type';
 import { PageInternal } from './type';
 import { ViewHistory } from './services/viewHistory';
@@ -10,18 +10,18 @@ export interface RouterContextObject {
   viewHistory: ViewHistory;
 }
 
-export const RouterContext = React.createContext<RouterContextObject>(null!);
+export const RouterContext = createContext<RouterContextObject>(null!);
 
 export interface RouteContextObject {
   state: RouterState;
   match?: AgnosticRouteMatch<string, PageInternal> | undefined;
-  panelsHistory?: string[];
+  panelsHistory: string[];
 }
 
-export const RouteContext = React.createContext<RouteContextObject>(null!);
+export const RouteContext = createContext<RouteContextObject>(null!);
 
 export interface PopoutContextObject {
   popout: JSX.Element | null;
 }
 
-export const PopoutContext = React.createContext<PopoutContextObject>({ popout: null });
+export const PopoutContext = createContext<PopoutContextObject>({ popout: null });
