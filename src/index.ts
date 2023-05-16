a2be3fb2dea50ad46939bccdaa1ba8a7b863b5ab
Nikita Shvyryaev <n.shvyryaev@vk.team>
2023-05-16 13:42:31 +0600

    MA-12425: make index.ts clean export only

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
-import { createHashHistory, createRouter, Router as RemixRouter } from '@remix-run/router';
-import { createKey } from './utils/utils';
-import { RouteWithoutRoot, RouteWithRoot } from './type';
-
-export type { RouteWithoutRoot, RouteWithRoot };
+export type { RouteWithoutRoot, RouteWithRoot } from './type';
 export type { RouterProviderProps } from './components/RouterProvider';
 export { RouterProvider } from './components/RouterProvider';
-export type { RouterContextObject } from './contexts';
+export { createHashRouter } from './utils/createHashRouter';
+
 export {
   useRouteNavigator,
   useParams,
@@ -14,22 +11,9 @@ export {
 export { useSearchParams } from './hooks/useSearchParams';
 export { useFirstPageCheck } from './hooks/useFirstPageCheck';
 export { useActiveVkuiLocation } from './hooks/useActiveVkuiLocation';
+
 export { withRouter } from './hoc/withRouter';
 
-export function createHashRouter(routes: RouteWithRoot[] | RouteWithoutRoot[]): RemixRouter {
-  // Задать новый key для новой локации в случае, если приложение уже запущено,
-  // а пользователь делает переход изменив hash в адресной строке браузера.
-  window.addEventListener('popstate', (event: PopStateEvent) => {
-    if (!event.state) {
-      window.history.replaceState({ key: createKey() }, '');
-    }
-  });
-  return createRouter({
-    history: createHashHistory(),
-    routes: routes.map((item) => ({ ...item, index: true })),
-  }).initialize();
-}
+export type { RouteNavigator } from './services/routeNavigator.type';
 
-export { AgnosticDataRouteObject, AgnosticRouteMatch } from '@remix-run/router';
-export { RouteNavigator } from './services/routeNavigator.type';
 export { RoutesConfig, createView, createRoot, createPanel, createModal } from './page-types';
---
+++ src/utils/createHashRouter.ts
@@ -0,0 +1,18 @@
+import { Router as RemixRouter } from '@remix-run/router/dist/router';
+import { createKey } from './utils';
+import { createHashHistory, createRouter } from '@remix-run/router';
+import { RouteWithoutRoot, RouteWithRoot } from '../type';
+
+export function createHashRouter(routes: RouteWithRoot[] | RouteWithoutRoot[]): RemixRouter {
+  // Задать новый key для новой локации в случае, если приложение уже запущено,
+  // а пользователь делает переход изменив hash в адресной строке браузера.
+  window.addEventListener('popstate', (event: PopStateEvent) => {
+    if (!event.state) {
+      window.history.replaceState({ key: createKey() }, '');
+    }
+  });
+  return createRouter({
+    history: createHashHistory(),
+    routes: routes.map((item) => ({ ...item, index: true })),
+  }).initialize();
+}
