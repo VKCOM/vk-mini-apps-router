926c419c498b210e718c6947d3940bffbf99bf63
Nikita Shvyryaev <n.shvyryaev@vk.team>
2023-07-05 20:08:26 +0600

    MA-13517: store path in hash parametr to enable use of other hash parts in the system

---
+++ src/utils/createHashParamRouter.ts
@@ -0,0 +1,21 @@
+import { Router as RemixRouter } from '@remix-run/router/dist/router';
+import { createKey } from './utils';
+import { createHashParamHistory, createRouter } from '@remix-run/router';
+import { RouteWithoutRoot, RouteWithRoot } from '../type';
+import { InitialLocation } from '../services/InitialLocation';
+
+export function createHashParamRouter(routes: RouteWithRoot[] | RouteWithoutRoot[]): RemixRouter {
+  // Задать новый key для новой локации в случае, если приложение уже запущено,
+  // а пользователь делает переход изменив hash в адресной строке браузера.
+  window.addEventListener('popstate', (event: PopStateEvent) => {
+    if (!event.state) {
+      window.history.replaceState({ key: createKey() }, '');
+    }
+  });
+  const history = createHashParamHistory();
+  InitialLocation.init(history.location);
+  return createRouter({
+    history,
+    routes: routes.map((item) => ({ ...item, index: true })),
+  }).initialize();
+}
