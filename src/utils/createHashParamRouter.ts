04114b88ca06dc94af7a7b157fd6e5cc928a7133
N <pasha-nikitin-2022@mail.ru>
2025-02-25 20:18:15 +0300

    MA-21406: fix links

--- src/utils/createHashParamRouter.ts
+++ src/utils/createHashParamRouter.ts
@@ -15,6 +15,19 @@ export function createHashParamRouter(routes: RouteWithRoot[] | RouteWithoutRoot
   });
   const history = createHashParamHistory();
   InitialLocation.init(history.location);
+  /*
+   * ВНИМАНИЕ:
+   * Если вы передаете пользовательский `paramName`, необходимо также обновить обработчик в RouterProvider,
+   * который подписывается на события bridge VKWebAppChangeFragment.
+   *
+   * Сейчас в обработчике используется жестко заданное имя параметра DEFAULT_PATH_PARAM_NAME:
+   *   const pathFromHash = hashParams.get(DEFAULT_PATH_PARAM_NAME);
+   *
+   * При использовании кастомного `paramName` эта строка должна быть изменена на:
+   *   const pathFromHash = hashParams.get(paramName);
+   *
+   * Это необходимо для корректной работы интеграции с VK Bridge.
+   */
   return createRouter({
     history,
     routes: routes.map((item) => ({ ...item, index: true })),
